27fdf5c7923555861e8a4f812f90a202119564dd
Yeakub1 <mryeakub10@gmail.com>
2024-03-12 12:37:41 +0600

    createMany

---
+++ src/create.ts
@@ -0,0 +1,44 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const main = async () => {
+  // create single data
+  //   const result = await prisma.post.create({
+  //     data: {
+  //       title: "This is a Title 3",
+  //       content: "This is a Content 2",
+  //       authorName: "Name Nay 2",
+  //     },
+  //   });
+  //     console.log(result)
+
+  const createMany = await prisma.post.createMany({
+    data: [
+      {
+        title: "title 1",
+        content: "This content 1",
+        authorName: "Yeakub",
+      },
+      {
+        title: "title 2",
+        content: "This content 2",
+        authorName: "Sayan",
+      },
+      {
+        title: "title 3",
+        content: "This content 3",
+        authorName: "Anik",
+      },
+      {
+        title: "title 4",
+        content: "This content 4",
+        authorName: "robin",
+      },
+    ],
+  });
+    
+    console.log(createMany)
+};
+
+main();
