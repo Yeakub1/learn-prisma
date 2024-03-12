334755fef83afa6290afdb741c672432387a060d
Yeakub1 <mryeakub10@gmail.com>
2024-03-12 14:38:14 +0600

    delete sinle and many and all

---
+++ src/delete.ts
@@ -0,0 +1,29 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const DeleteData = async () => {
+
+    // single delete
+  // const singleDele = await prisma.post.delete({
+  //     where: {
+  //         id: 5
+  //     }
+  // })
+  // console.log(singleDele);
+
+    // many data delete
+  const manyDelete = await prisma.post.deleteMany({
+    where: {
+      published: false,
+    },
+  });
+  console.log(manyDelete);
+
+    
+    // all data delete
+  const allDelete = await prisma.post.deleteMany({});
+  console.log(allDelete);
+};
+
+DeleteData();
