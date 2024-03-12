06a14db4f5eba3c05179c603b0c2add920b689e8
Yeakub1 <mryeakub10@gmail.com>
2024-03-12 11:24:52 +0600

    post and findMany data

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log("yekaub");
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const main = async () => {
+//   const result = await prisma.post.create({
+//     data: {
+//       title: "This is a Title 3",
+//       content: "This is a Content 2",
+//       authorName: "Name Nay 2",
+//     },
+//   });
+    //     console.log(result)
+    
+    const getAllFormDB = await prisma.post.findMany();
+    console.log(getAllFormDB)
+};
+
+main();
