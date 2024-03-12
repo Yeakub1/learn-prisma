8dc7d5ec16db680d573775258e6c3b6a08045002
Yeakub1 <mryeakub10@gmail.com>
2024-03-12 12:59:59 +0600

    update many

--- src/update.ts
+++ src/update.ts
@@ -9,11 +9,21 @@ const updates = async () => {
     },
     data: {
       title: "Title 220",
-    //   content: "Content 20",
-    //   authorName: "Tumi aso tumi nay",
+      //   content: "Content 20",
+      //   authorName: "Tumi aso tumi nay",
     },
   });
-    console.log(singleUpdate)
+  // console.log(singleUpdate)
+
+  const updateMany = await prisma.post.updateMany({
+    where: {
+      title: "title 2",
+    },
+    data: {
+     published: true
+    },
+  });
+  console.log(updateMany);
 };
 
 updates();
