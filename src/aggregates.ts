19707db3506d55821e537e887c971d79acfad619
Yeakub1 <mryeakub10@gmail.com>
2024-03-19 12:13:49 +0600

    groupBy

--- src/aggregates.ts
+++ src/aggregates.ts
@@ -3,12 +3,51 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 const aggregates = async () => {
+  // find aggregates age
   const avgAge = await prisma.user.aggregate({
     _avg: {
       age: true,
     },
   });
-  console.log(avgAge);
+  // console.log(avgAge);
+
+  // find sub age
+  const sumAge = await prisma.user.aggregate({
+    _sum: {
+      age: true,
+    },
+  });
+  // console.log(sumAge);
+
+  //  find cound of age fildes
+  const count = await prisma.user.aggregate({
+    _count: {
+      age: true,
+    },
+  });
+  console.log(count);
+
+  // find number of recods
+
+  const countData = await prisma.user.count();
+
+  // console.log(countData);
+
+  //  find max age
+  const maxAge = await prisma.user.aggregate({
+    _max: {
+      age: true,
+    },
+  });
+  console.log(maxAge);
+
+  //  find max age
+  const minAge = await prisma.user.aggregate({
+    _min: {
+      age: true,
+    },
+  });
+  console.log(minAge);
 };
 
 aggregates();
---
+++ src/groupBy.ts
@@ -0,0 +1,18 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const groupBy = async () => {
+  const groupPost = await prisma.post.groupBy({
+    by: "published",
+    _count: {
+      title: true,
+    },
+    having: {
+      published: true,
+    },
+  });
+  console.log(groupPost);
+};
+
+groupBy();
